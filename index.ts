8c2101b1b639c341698aa9989bd74fdd77b958e6
Rafal Zw <zwierz.rafal@gmail.com>
2022-06-03 23:06:16 +0200

    Added error handling.

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
 import express, {json} from "express";
+import {handleError, ValidationError} from "./utils/errors";
 
 const app = express();
 
@@ -7,6 +8,9 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(json());
+
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3001');
