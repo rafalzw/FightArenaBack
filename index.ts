f76fe6284b1e8c9e9258df39696c235f35a06fb8
Rafal Zw <zwierz.rafal@gmail.com>
2022-06-04 11:53:43 +0200

    Added addRouter.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, {json} from "express";
 import {handleError, ValidationError} from "./utils/errors";
+import {addRouter} from "./routes/create-warrior";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors({
 }));
 app.use(json());
 
+app.use('/create-warrior', addRouter);
 
 app.use(handleError);
 
